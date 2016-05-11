39226a435b89be6147496b1a2166fb62964a0fdc
dkoruba <dawid.koruba@thecogworks.co.uk>
2016-05-11 13:58:54 +0200

    init commit

---
+++ UmbracoTest/App_Plugins/Example/charlimit.controller.ts
@@ -0,0 +1,27 @@
+﻿angular.module("umbraco")
+    .controller("Example.CharLimitController", [
+        '$scope',
+        ($scope) => {
+            var limit = $scope.model.config.limit;
+
+            $scope.limitchars = () => {
+                if (!$scope.model.value)
+                    return;
+
+                if ($scope.model.value.length > limit) {
+                    $scope.model.value = $scope.model.value.substr(0, limit);
+                }
+            }
+
+            $scope.$watch('model.value', () => {
+                if (!$scope.model.value)
+                    return;
+
+                if ($scope.model.value.length >= limit) {
+                    $scope.info = "You cannot write more than " + limit + " characters.";
+                } else {
+                    $scope.info = "You have " + (limit - $scope.model.value.length) + " characters left.";
+                }
+            }, true);
+        }
+    ]);
