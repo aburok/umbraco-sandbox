080c156fdeec1d3434864382e30492bd010b0018
dkoruba <dawid.koruba@thecogworks.com>
2016-05-30 23:17:20 +0200

    Save at 2016-05-30 23:17:20Z

---
+++ UmbracoTest/Controllers/RegisterUserController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using UmbracoTest.ViewModels;
+
+namespace UmbracoTest.Controllers
+{
+    public class RegisterUserController : SurfaceController
+    {
+        // GET: Membership
+        public ActionResult MemberRegister(RegisterUserViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UmbracoTest/ViewModels/RegisterUserViewModel.cs
+++ UmbracoTest/ViewModels/RegisterUserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,13 +9,21 @@ namespace UmbracoTest.ViewModels
 {
     public class RegisterUserViewModel
     {
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string EmailAddress { get; set; }
 
+        [Required]
         [Remote("ValidatePassword", "Validation", HttpMethod = "POST", AdditionalFields = "EmailAddress, Username")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
         public string PasswordConfirmation { get; set; }
     }
 }
